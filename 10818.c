649e15bc2c7515a295f57ce77b56782773b8304f
JunSeong An <ajs3801@g.skku.edu>
2022-04-29 00:47:59 +0900

    Update 10818.c

--- 10818.c
+++ 10818.c
@@ -1,11 +1,17 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
+//#include <stdlib.h> //malloc,free 함수 들어가있음
 
 int main(void)
 {
 	int array[1000000];
 	int cnt; int i = 0; int j = 0;
 	scanf("%d", &cnt);
+	
+	//int* array = (int*)malloc(sizeof(int)*cnt); 
+	//* cnt에서 입력받은 만큼 배열 할당 가능
+	//* int array[1000000]; 대체 가능
+	
 	for (i; i < cnt; i++)
 	{
 		int a;
@@ -22,6 +28,9 @@ int main(void)
 			min = array[j];
 	}
 	printf("%d %d", min, max);
-
+	
+	//free(array);
+	//* 동적할당 받은 array변수 deallocate
+	
 	return 0;
 }
